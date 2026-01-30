e9b8c23b0ca555c58eeebfe6160cf8d6e42b506e
sandy <sandyezzat70@gmail.com>
2026-01-30 20:27:38 +0100

    add new code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-
-const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/makeup-tracker")
@@ -11,12 +12,14 @@ mongoose
 
 app.use(express.json()); // allows JSON body in POST
 
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Makeup Price Tracker API is running");
+});
+
 const PORT = 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.status(200).send("Makeup Price Tracker API is running");
 });
---
+++ routes/productRouts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Product = require("../models/product");
+
+const router = express.Router();
+
+router.post("/api/products", async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add product" });
+  }
+});
+
+router.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+});
+
+module.exports = router;
